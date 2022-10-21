9fe0942ce5536056b7bdbeda60465f5a3e6475bb
anthonysilva98 <anthonyssilva98@gmail.com>
2022-10-21 15:18:20 -0700

    Create Restaurant Item and Import to home

--- screens/Home.js
+++ screens/Home.js
@@ -1,8 +1,9 @@
-import { View, Text, SafeAreaView } from "react-native";
+import { View, Text, SafeAreaView, ScrollView } from "react-native";
 import React from "react";
 import HeaderTabs from "../components/HeaderTabs";
 import SearchBar from "../components/SearchBar";
 import Catagories from "../components/Catagories";
+import RestaurantItem from "../components/RestaurantItem";
 const Home = () => {
   return (
     <SafeAreaView style={{ backgroundColor: "grey", flex: 1 }}>
@@ -10,7 +11,10 @@ const Home = () => {
         <HeaderTabs />
         <SearchBar />
       </View>
-      <Catagories />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Catagories />
+        <RestaurantItem />
+      </ScrollView>
     </SafeAreaView>
   );
 };
