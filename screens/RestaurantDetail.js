a9e24231158bf6ff0995c215299acc1f67bba501
anthonysilva98 <anthonyssilva98@gmail.com>
2022-11-06 21:47:50 -0800

    Add route information to About Screen

--- screens/RestaurantDetail.js
+++ screens/RestaurantDetail.js
@@ -3,10 +3,10 @@ import React from "react";
 import { Divider } from "react-native-elements";
 import About from "../components/restaurantDetail/About";
 import MenuItem from "../components/restaurantDetail/MenuItem";
-const RestaurantDetail = () => {
+const RestaurantDetail = ({ route }) => {
   return (
     <View>
-      <About />
+      <About route={route} />
       <Divider width={1.8} style={{ marginVertical: 20 }} />
       <MenuItem />
     </View>
