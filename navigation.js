d736536db77de012abf6e80a9eb437edc176f478
anthonysilva98 <anthonyssilva98@gmail.com>
2022-11-06 21:46:03 -0800

    Create navigation Component for screens and info

--- navigation.js
+++ navigation.js
@@ -1,2 +1,23 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import Home from "./screens/Home";
+import RestaurantDetail from "./screens/RestaurantDetail";
+import { View, Text } from "react-native";
+
+const RootNavigation = () => {
+  const Stack = createStackNavigator();
+  const screenOptions = {
+    headerShown: false,
+  };
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={screenOptions} initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="RestaurantDetail" component={RestaurantDetail} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default RootNavigation;
