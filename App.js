4aed8fb3a2733f586e575f3bea45529b13da696f
anthonysilva98 <anthonyssilva98@gmail.com>
2022-11-06 21:45:26 -0800

    Use Root navigator in home to show screens

--- App.js
+++ App.js
@@ -1,6 +1,4 @@
-import { View, Text } from "react-native";
-import Home from "./screens/Home";
-import RestaurantDetail from "./screens/RestaurantDetail";
+import RootNavigation from "./navigation";
 export default function App() {
-  return <RestaurantDetail />;
+  return <RootNavigation />;
 }
