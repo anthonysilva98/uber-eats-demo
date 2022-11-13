a881c50efff97bae4ba00dcfc03516acf317ce73
anthonysilva98 <anthonyssilva98@gmail.com>
2022-11-13 15:26:13 -0800

    Add View cart button to details screen

--- screens/RestaurantDetail.js
+++ screens/RestaurantDetail.js
@@ -3,12 +3,15 @@ import React from "react";
 import { Divider } from "react-native-elements";
 import About from "../components/restaurantDetail/About";
 import MenuItem from "../components/restaurantDetail/MenuItem";
-const RestaurantDetail = ({ route }) => {
+import ViewCart from "../components/restaurantDetail/ViewCart";
+const RestaurantDetail = ({ route, navigation }) => {
   return (
     <View>
       <About route={route} />
       <Divider width={1.8} style={{ marginVertical: 20 }} />
       <MenuItem />
+
+      <ViewCart navigation={navigation} restaurantName={route.params.name} />
     </View>
   );
 };
