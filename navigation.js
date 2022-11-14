4f0c01db4f34691d02bb1b3cac963e081972208a
anthonysilva98 <anthonyssilva98@gmail.com>
2022-11-13 18:04:29 -0800

    Fix bugs with libraries and added redux toolkit

--- navigation.js
+++ navigation.js
@@ -3,19 +3,24 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import Home from "./screens/Home";
 import RestaurantDetail from "./screens/RestaurantDetail";
+import { Provider as ReduxProvider } from "react-redux";
+import configureStore from "./redux/store";
 
+const store = configureStore();
 const RootNavigation = () => {
   const Stack = createStackNavigator();
   const screenOptions = {
     headerShown: false,
   };
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={screenOptions} initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="RestaurantDetail" component={RestaurantDetail} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <ReduxProvider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={screenOptions} initialRouteName="Home">
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="RestaurantDetail" component={RestaurantDetail} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ReduxProvider>
   );
 };
 
