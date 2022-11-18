97100a0fa7edfe1b5f1197411b8df187ceb181a5
anthonysilva98 <anthonyssilva98@gmail.com>
2022-11-18 14:22:20 -0800

    Add new reducer to remove items from store

--- redux/reducers/cartReducer.js
+++ redux/reducers/cartReducer.js
@@ -4,15 +4,30 @@ let defaultState = {
 
 let cartReducer = (state = defaultState, action) => {
   switch (action.type) {
-    case "Add_To_Cart": {
+    case "ADD_TO_CART": {
       let newState = { ...state };
-      newState.selectedItems = {
-        items: [...newState.selectedItems.items, action.payload],
-        restaurantName: action.payload.restaurantName,
-      };
-      console.log(newState, "New State");
+
+      if (action.payload.checkboxValue) {
+        console.log("ADDED TO CART");
+        newState.selectedItems = {
+          items: [...newState.selectedItems.items, action.payload],
+          restaurantName: action.payload.restaurantName,
+        };
+      } else {
+        console.log("REMOVED FROM CART");
+        newState.selectedItems = {
+          items: [
+            ...newState.selectedItems.items.filter(
+              (item) => item.title != action.payload.title
+            ),
+          ],
+          restaurantName: action.payload.restaurantName,
+        };
+      }
+      console.log(newState.selectedItems, "New State");
       return newState;
     }
+
     default:
       return state;
   }
