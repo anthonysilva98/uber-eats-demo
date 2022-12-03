09e5e07b3cb995e6be795b2240a5643d15a6de8e
anthonysilva98 <anthonyssilva98@gmail.com>
2022-12-02 18:39:44 -0800

    Add order completed to navigation

--- navigation.js
+++ navigation.js
@@ -5,6 +5,7 @@ import Home from "./screens/Home";
 import RestaurantDetail from "./screens/RestaurantDetail";
 import { Provider as ReduxProvider } from "react-redux";
 import configureStore from "./redux/store";
+import OrderCompleted from "./screens/OrderCompleted";
 
 const store = configureStore();
 const RootNavigation = () => {
@@ -18,6 +19,7 @@ const RootNavigation = () => {
         <Stack.Navigator screenOptions={screenOptions} initialRouteName="Home">
           <Stack.Screen name="Home" component={Home} />
           <Stack.Screen name="RestaurantDetail" component={RestaurantDetail} />
+          <Stack.Screen name="OrderCompleted" component={OrderCompleted} />
         </Stack.Navigator>
       </NavigationContainer>
     </ReduxProvider>
